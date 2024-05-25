dfd332665faf6346d97cd024fb42633fff69bd7e
Dor Eden <jdog1075@gmail.com>
2024-05-25 20:23:50 +0300

    import fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { Provider } from "react-redux";
 import store from "./store";
 import MainLayout from "./components/Layout";
-import { pageConditions } from "./helpers/consts";
 
 function App() {
   return (
